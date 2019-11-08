bfb5d39ab8af0d3e0f842ec0d6786a6191c83fdc
Juan Camilo Naranjo <juanca.naranjo03@gmail.com>
2019-11-08 11:08:21 -0500

    cosas menores

--- js/config/main.config.js
+++ js/config/main.config.js
@@ -2,6 +2,7 @@ let config = {
     USER_DB: "postgres",
     HOST_DB: "localhost",
     DATABASE: "Convenios",
+    //esta clave es especifica de cada uno, es decir, es de su proyecto y su pc, tal como configuró PG
     PASSWORD_DB: "admin",
     PORT_DB: 5432,
     SECRET_KEY: "MicLavEsEcreta123#!"
